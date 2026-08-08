d3b6628405185b68c25ac2630faa8c2426e980b8
Conte22 <142271670+Conte22@users.noreply.github.com>
2026-08-07 21:09:05 -0300

    Botão Colocado

--- aula3107/aula31072/App.js
+++ aula3107/aula31072/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Boa Noite !</Text>
+      <Button
+      title="Clique se quiser"
+      color="#00570c"
+      accessibilityLabel="Clique se quiser"
+      />
       <StatusBar style="auto" />
     </View>
   );
